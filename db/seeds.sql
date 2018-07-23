3f3c44de8c682250cb60b4b3bdc9e89a3402a478
Wulfsbach <etemelkos@gmail.com>
2018-07-23 13:37:36 -0400

    50%

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers(burger_name,devoured) VALUES ("Smokehouse Burger", FALSE);
+INSERT INTO burgers(burger_name,devoured) VALUES ("Mozerella Burger", FALSE);
+INSERT INTO burgers(burger_name,devoured) VALUES ("Bison Burger", FALSE);
